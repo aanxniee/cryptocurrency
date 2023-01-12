84b3a00fc34f26252174d600a970b57b579f6213
Annie Cai <caishajia@gmail.com>
2023-01-12 14:40:49 -0500

    added api services

---
+++ src/app/service/api.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http : HttpClient) { }
+
+  getCurrencyData(currency:string) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false    `);
+  }
+
+  getGraphData(coinId:string, currency:string, days:string) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency}&days=${days}`)
+  }
+
+  getCurrencyById(coinId:string) {
+    return this.http.get<any>(`https://api.coingecko.com/api/v3/coins/${coinId}`)
+  }
+}
