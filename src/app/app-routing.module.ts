053f029dbd3a9087999c4918ba3597f49cee892f
Annie Cai <caishajia@gmail.com>
2023-01-12 13:58:54 -0500

    added routing to the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CoinDetailsComponent } from './coin-details/coin-details.component';
+import { CoinsComponent } from './coins/coins.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo : 'coins', pathMatch:'full'},
+  {path: 'coins', component: CoinsComponent},
+  {path: 'coin-details', component: CoinDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
