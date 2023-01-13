7d40808dc424ac1580504dc1a53f7a636dff7507
Annie Cai <caishajia@gmail.com>
2023-01-13 10:52:45 -0500

    fetched graph data from api

--- src/app/coin-details/coin-details.component.ts
+++ src/app/coin-details/coin-details.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../service/api.service';
 import { ChartConfiguration, ChartType } from 'chart.js';
-import {BaseChartDirective} from 'ng2-charts';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-coin-details',
@@ -14,7 +14,7 @@ export class CoinDetailsComponent implements OnInit {
 
   coinData: any;
   coinId!: string;
-  days: number = 1;
+  days: number = 30;
   currency: string = "CAD";
 
   public lineChartData: ChartConfiguration['data'] = {
@@ -54,9 +54,10 @@ export class CoinDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(val=>{
       this.coinId = val['id'];
-    })
+    });
   
     this.getCoinData();
+    this.getGraphData(this.days);
   }
 
   getCoinData() {
@@ -64,13 +65,26 @@ export class CoinDetailsComponent implements OnInit {
     .subscribe(res=>{
       this.coinData=res;
       console.log(this.coinData);
-    })
+    });
   }
 
-  getGraphData() {
-    this.api.getGraphCurrencyData(this.coinId, "CAD", 1)
+  getGraphData(days:number) {
+    this.days = days;
+    this.api.getGraphCurrencyData(this.coinId, "CAD", this.days)
     .subscribe(res=>{
-      console.log(res);
+      this.lineChartData.datasets[0].data = res.prices.map((a:any)=>{
+        return a[1];
+      });
+
+      this.lineChartData.labels = res.prices.map((a:any)=>{
+        let date = new Date(a[0]);
+        let time = date.getHours() > 12 ? 
+        `${date.getHours() - 12}: ${date.getMinutes()} PM` :
+        `${date.getHours()}: ${date.getMinutes()} AM`
+        console.log(time);
+        console.log(date);
+        return this.days === 1 ? time : date.toLocaleDateString();
+      })
     })
   }
 }
