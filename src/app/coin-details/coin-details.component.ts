5e03def4c27a567c1b4dc720f225da86fa4bb52b
Annie Cai <caishajia@gmail.com>
2023-01-13 11:16:28 -0500

    added currency change feature to coin details page

--- src/app/coin-details/coin-details.component.ts
+++ src/app/coin-details/coin-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ApiService } from '../service/api.service';
 import { ChartConfiguration, ChartType } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
+import { CurrencyService } from '../service/currency.service';
 
 @Component({
   selector: 'app-coin-details',
@@ -45,11 +46,9 @@ export class CoinDetailsComponent implements OnInit {
     }
   };
   public lineChartType: ChartType = 'line';
-
   @ViewChild(BaseChartDirective) myLineChart !: BaseChartDirective;
 
-
-  constructor(private api : ApiService, private activatedRoute : ActivatedRoute) {}
+  constructor(private api : ApiService, private activatedRoute : ActivatedRoute, private currencyService : CurrencyService) {}
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(val=>{
@@ -58,11 +57,23 @@ export class CoinDetailsComponent implements OnInit {
   
     this.getCoinData();
     this.getGraphData(this.days);
+    this.currencyService.getCurrency()
+    .subscribe(val=>{
+      this.currency = val;
+      this.getCoinData();
+      this.getGraphData(this.days);
+    })
   }
 
   getCoinData() {
     this.api.getCurrencyById(this.coinId)
     .subscribe(res=>{
+      if(this.currency === "USD"){
+        res.market_data.current_price.cad = res.market_data.current_price.usd;
+        res.market_data.market_cap.cad = res.market_data.market_cap.usd;
+      }
+      res.market_data.current_price.cad = res.market_data.current_price.cad;
+      res.market_data.market_cap.cad = res.market_data.market_cap.cad;
       this.coinData=res;
       console.log(this.coinData);
     });
@@ -70,7 +81,7 @@ export class CoinDetailsComponent implements OnInit {
 
   getGraphData(days:number) {
     this.days = days;
-    this.api.getGraphCurrencyData(this.coinId, "CAD", this.days)
+    this.api.getGraphCurrencyData(this.coinId, this.currency, this.days)
     .subscribe(res=>{
       setTimeout(()=> {
         this.myLineChart.chart?.update();
